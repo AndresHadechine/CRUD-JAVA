6b3ba55870b2436ae9e209972187d09dceeb435b
mackbook pro <mackbookpro@MacBook-Pro-de-mackbook.local>
2021-03-02 11:44:32 -0500

    Se hizo la configuración inicial del proyecto, tanto en el back como en el front

---
+++ Back-CRUD/src/test/java/co/com/sofka/BackCRUD/BackCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.BackCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
